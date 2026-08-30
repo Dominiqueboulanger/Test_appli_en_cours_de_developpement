f238ef21cf970da608d5efdffdc815e55304733c
Dominiqueboulanger <dominiqueboulanger@wanadoo.fr>
2026-08-30 11:21:25 +0200

    Update server.py

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 import os
 import uvicorn
+from wsgi import app
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
-    # On lance Uvicorn en ciblant le module wsgi:app
-    uvicorn.run("wsgi:app", host="0.0.0.0", port=port, log_level="info")
+    uvicorn.run(app, host="0.0.0.0", port=port, log_level="info")
