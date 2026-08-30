4fe5e082a9fc4d3fc6fc39fd0d36578703ebff66
Dominiqueboulanger <dominiqueboulanger@wanadoo.fr>
2026-08-30 10:55:44 +0200

    Initialize WSGI application with necessary imports
    Import necessary modules and configure for production.

---
+++ wsgi.py
@@ -0,0 +1,6 @@
+import os
+from snapeval import *  # Importe toute votre application NiceGUI
+from nicegui import app
+
+# Configuration pour le serveur de production
+# (Pas de ui.run ici, Uvicorn va piloter l'application directement)
